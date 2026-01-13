c430c687c8de43afe0c055ce792aeaca8bae3529
Stas Koshynskyi <stasony100@gmail.com>
2026-01-13 09:23:40 +0200

    додав порівняння

--- src/main/java/lection1/Main.java
+++ src/main/java/lection1/Main.java
@@ -7,7 +7,18 @@ public class Main {
         Dog doge = new Dog ("asd", "qwe");
         System.out.println(doge);
 
-        Animal dog = new Dog ("asd", "qwe");
+        Dog dog = new Dog ("asd", "qwe");
+
+        Dog anotherDog = new Dog ("aaa", "eee");
+
+
+        System.out.println(doge == dog);
+        System.out.println(dog.equals(dog));
+
+        System.out.println(dog.equals(anotherDog));
+        System.out.println(dog == anotherDog);
+
+        Animal animalDog = new Dog ("asd", "qwe");
 
         Animal animal = new Animal("qqqqq");
 
